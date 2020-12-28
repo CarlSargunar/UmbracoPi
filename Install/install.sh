ed040a3100632974b03448f63ca576d74d149eea
Carl Sargunar <carl@sargunar.com>
2020-12-28 18:16:54 +0000

    wip

--- Install/install.sh
+++ Install/install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+# Based on : https://github.com/pjgpetecodes/dotnetcore3pi/
 
 echo -e "\e[1m----------------------------------------"
 echo -e "\e[1m       Dot Net Core Installer"
 echo -e "\e[1m----------------------------------------"
-echo ""
-echo -e "\e[1mPete Codes / PJG Creations 2020"
-echo ""
-echo -e "Latest update 01/10/2020"
 echo ""
 echo "This will install the following;"
 echo ""
@@ -16,14 +13,6 @@ echo "- Dot Net Core 3.1.404"
 echo "- ASP.NET Core 3.1.10"
 echo "- Blazor WASM 3.2.1 (Included automatically)"
 echo ""
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-echo -e "Any suggestions or questions, email \e[1;4mpete@pjgcreations.co.uk"
-echo -e "\e[0mSend me a tweet \e[1;4m@pete_codes"
-echo -e "\e[0mTutorials on \e[1;4mhttps://www.petecodes.co.uk"
-echo ""
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
 
 if [[ $EUID -ne 0 ]]; then
    echo -e "\e[1;31mThis script must be run as root" 
@@ -108,29 +97,7 @@ fi
 
 echo -e "\e[0m"
 echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m   Blazor Templates are now included!"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-
-#dotnet new -i Microsoft.AspNetCore.Components.WebAssembly.Templates::3.2.1
-
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m          Run dotnet --info"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-
-dotnet --info
-
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
 echo -e "\e[1m              ALL DONE!"
-echo ""
-echo -e "\e[0mGo ahead and run \e[1mdotnet new console \e[0min a new directory!"
-echo ""
-echo ""
-echo -e "Let me know how you get on by tweeting me at \e[1;5m@pete_codes"
-echo ""
 echo -e "\e[1m----------------------------------------"
 echo -e "\e[0m"
 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # UmbracoPi
 
 This is the repsitory for running the Umbraco Alpha code on a Raspberry Pi, intended for the Skrift Article January 2021
+
+This article depends on a version of the install script from Pete Gallagher's repository : [https://github.com/pjgpetecodes/dotnetcore3pi/install.sh](https://github.com/pjgpetecodes/dotnetcore3pi/install.sh)
